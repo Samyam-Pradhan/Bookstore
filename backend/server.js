7c30310960c9b63dbdc30cf85eea5445e776a723
Samyam-Pradhan <pradhansamyam71@gmail.com>
2026-01-23 18:19:05 +0545

    Add user model and auth contorller

--- backend/server.js
+++ backend/server.js
@@ -8,14 +8,11 @@ dotenv.config();
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Routes
 app.use("/api/auth", authRoutes);
 
-// DB connection
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
