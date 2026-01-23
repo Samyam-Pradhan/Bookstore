125850b365eb01f0db3146bddb7a47df29ce26f5
Samyam-Pradhan <pradhansamyam71@gmail.com>
2026-01-23 17:15:46 +0545

    Initial files for express

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,27 @@
-const express = require("express");
-const dotenv = require("dotenv");
-/* const connectDb = require("./config/db"); */
-const bookRoutes = require("./routes/bookRoutes");
-const userRoutes = require("./routes/authRoutes");
-const cors = require("cors");
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import authRoutes from "./routes/authRoutes.js";
 
-dotenv.config(); // Load environment variables
-//connectDb(); // Connect to MongoDB
+dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors()); // Allow requests from frontend (avoids CORS issues)
+app.use(cors());
 app.use(express.json());
 
 // Routes
-app.use('/api/v1', userRoutes);
-app.use('/api/books', bookRoutes); 
+app.use("/api/auth", authRoutes);
 
+// DB connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error(err));
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
