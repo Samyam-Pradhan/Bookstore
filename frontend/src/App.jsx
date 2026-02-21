2dae2e129d97cc64abe2e8e0bacab9502dc4257f
Samyam Pradhan <pradhansamyam71@gmail.com>
2026-02-21 14:31:56 +0545

    Add login and signup modal

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React from "react";
 import Homepage from "./pages/Hompage";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
 import Shop from "./pages/Shop";
 
 function App() {
@@ -10,8 +8,6 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
         <Route path="/shop" element={<Shop />} />
       </Routes>
     </Router>
--- frontend/src/pages/Hompage.jsx
+++ frontend/src/pages/Hompage.jsx
@@ -1,21 +1,53 @@
-import React from 'react';
-import Navbar from '../components/Navbar';
-import Hero from '../components/Hero';
-import Category from '../components/Category';
-import Feature from '../components/Feature'
-import Footer from '../components/Footer';
-import BestSelling from '../components/BestSelling';
+import React, { useState } from "react";
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import Category from "../components/Category";
+import Feature from "../components/Feature";
+import Footer from "../components/Footer";
+import BestSelling from "../components/BestSelling";
+import AuthModal from "../components/AuthModal";
+import Login from "./Login";
+import Signup from "./Signup";
 
 function Homepage() {
+  const [showLogin, setShowLogin] = useState(false);
+  const [showSignup, setShowSignup] = useState(false);
+
+  // Called when login/signup is successful
+  const handleAuthSuccess = () => {
+    setShowLogin(false);
+    setShowSignup(false);
+    // You can also trigger Navbar update via a state or context
+  };
+
   return (
     <>
-      <Navbar/>
+      <Navbar
+        onLoginClick={() => setShowLogin(true)}
+        onSignupClick={() => setShowSignup(true)}
+      />
+
       <Hero />
       <Feature />
       <Category />
       <BestSelling />
       <Footer />
-     
+
+      {/* Login Modal */}
+      {showLogin && (
+        <Login
+          onSuccess={handleAuthSuccess}
+          onClose={() => setShowLogin(false)}
+        />
+      )}
+
+      {/* Signup Modal */}
+      {showSignup && (
+        <Signup
+          onSuccess={handleAuthSuccess}
+          onClose={() => setShowSignup(false)}
+        />
+      )}
     </>
   );
 }
